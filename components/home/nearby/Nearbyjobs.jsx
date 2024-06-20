9e5b45824f093333e057417a7f5ad5e54a174bf2
obviously_ploy <basekacarl@gmail.com>
2024-06-20 18:18:42 -0400

    Implemented the Location based search for Nearby Jobs

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,17 +1,39 @@
-import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
-import { useRouter } from 'expo-router'
-import useFetch from '../../../hook/useFetch'
-
-import styles from './nearbyjobs.style'
-import {COLORS} from "../../../constants";
-import  NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import useLocation from '../../../hook/useLocation';
+import useFetch from '../../../hook/useFetch';
+import styles from './nearbyjobs.style';
+import { COLORS } from "../../../constants";
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
 
 const Nearbyjobs = () => {
   const router = useRouter();
-  const { data, isLoading, error} = useFetch('search', {
-    query: 'React Developer', 
+
+  const [query, setQuery] = useState('React Developer');
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const { location, address, isLocationLoading, locationError } = useLocation();
+
+  const { data: searchData, isLoading: isSearchLoading, error: searchError, refetch } = useFetch('search', {
+    query: query,
     num_pages: 1
-  });
+  }, shouldFetch);
+
+  useEffect(() => {
+    if (address) {
+      const newQuery = `React Developer in ${address.city}, ${address.isoCountryCode}`;
+      setQuery(newQuery);
+      setShouldFetch(true);  
+    }
+  }, [address]);
+
+  useEffect(() => {
+    if (searchData || searchError) {
+      setShouldFetch(false); 
+      console.log("Search Data:", searchData);
+      console.log("Search Error:", searchError);
+    }
+  }, [searchData, searchError]);
 
   return (
     <View style={styles.container}>
@@ -23,22 +45,22 @@ const Nearbyjobs = () => {
       </View>
 
       <View style={styles.cardsContainer}>
-        {isLoading ?(
-          <ActivityIndicator size='large' colors={COLORS.primary}/>
-        ) : error ? (
+        {(isSearchLoading || isLocationLoading) ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : (searchError) ? (
           <Text>Something Went Wrong</Text>
         ) : (
-          data?.map((job) => (
+          searchData?.map((job) => (
             <NearbyJobCard
-              job = {job}
-              key = {`nearby-job-${job?.job_id}`}
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
               handleNavigate={() => router.push(`/job-details/${job?.job_id}`)}
             />
           ))
         )}
       </View>
     </View>
-  )
+  );
 }
 
-export default Nearbyjobs
+export default Nearbyjobs;
