f3e790d8e18b4d8e07fbeae37af2ae4cc7dbdade
obviously_ploy <basekacarl@gmail.com>
2024-07-07 20:06:14 -0400

    Finished the frontend view of the login page

--- app/login/Login.jsx
+++ app/login/Login.jsx
@@ -3,22 +3,39 @@ import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
 import styles from './login.style'
 import { COLORS, icons, images } from "../../constants";
 import LoginPageField from '../../components/common/cards/login/LoginPageField';
+import {Stack} from 'expo-router';
 
 
 const LoginPage = () => {
 
     return(
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <LoginPageField
-                iconUrl = {icons.email}
-                fieldName = {'Email'}
-                isSecureText = {false}
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerTitle: ""
+                }}
             />
-            <LoginPageField
-                iconUrl = {icons.padlock}
-                fieldName = {'Password'}
-                isSecureText = {true}
-            />
+            <View style={styles.container}>
+                <LoginPageField
+                    iconUrl = {icons.email}
+                    fieldName = {'Email'}
+                    isSecureText = {false}
+                />
+                <LoginPageField
+                    iconUrl = {icons.padlock}
+                    fieldName = {'Password'}
+                    isSecureText = {true}
+                />
+                <TouchableOpacity>
+                    <View style={styles.buttonContainer}>
+                        <Text style={styles.buttonText}>Login</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            
         </SafeAreaView>
     )
 }
--- components/common/cards/login/loginpagefield.style.js
+++ components/common/cards/login/loginpagefield.style.js
@@ -3,36 +3,29 @@ import { StyleSheet } from "react-native";
 import { COLORS, FONT, SIZES } from "../../../../constants";
 
 const styles = StyleSheet.create({
-    fieldContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-        marginTop: SIZES.large,
-        height: 50,
-      },
-      fieldWrapper: {
-        flex: 1,
-        backgroundColor: COLORS.white,
-        marginRight: SIZES.small,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: SIZES.medium,
-        height: "100%",
-      },
-      fieldInput: {
-        fontFamily: FONT.regular,
-        width: "100%",
-        height: "100%",
-        paddingHorizontal: SIZES.medium,
-      },
-      fieldIcon: {
-        width: 20,
-        height: 20,
-        justifyContent: "center",
-        alignItems: "center",
-        marginRight: SIZES.small,
-        opacity: 0.5
-    }
+  fieldContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: 'auto',
+    marginBottom: SIZES.medium,
+    backgroundColor: COLORS.white,
+  },
+  fieldInput: {
+    fontFamily: FONT.regular,
+    fontSize: SIZES.medium,
+    color: COLORS.gray,
+    width: "100%",
+    height: "100%",
+  },
+  fieldIcon: {
+    width: 20,
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: SIZES.small,
+    opacity: 0.5
+  }
 
 })
 
