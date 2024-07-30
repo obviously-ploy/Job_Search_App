992b1c3f7bab905a2c9b8c9a9ef4b209b3da0fc5
obviously_ploy <basekacarl@gmail.com>
2024-07-29 20:53:12 -0400

    Resolved the user creation issue

--- app/sign-up/SignUpScreen.jsx
+++ app/sign-up/SignUpScreen.jsx
@@ -1,89 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import { View, SafeAreaView, Text} from 'react-native';
-import { Stack, useRouter } from 'expo-router';
-import { COLORS, icons} from "../../constants";
+import React, { useState } from 'react';
+import { View, SafeAreaView, Text, ActivityIndicator, Alert } from 'react-native';
+import { Stack } from 'expo-router';
+import { COLORS, icons } from "../../constants";
 import styles from './signUpScreen.style';
-import SignUpScreenInfo from "../../components/common/cards/sign-up/SignUpScreenInfo"
+import SignUpScreenInfo from "../../components/common/cards/sign-up/SignUpScreenInfo";
 import LoginScreenBtn from '../../components/common/cards/login/LoginScreenBtn';
 import useHandleSignUp from '../../utils/useHandleSignUp';
-import useValidateSignUp from '../../utils/useValidateSignUp';
 
 const SignUpScreen = () => {
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [errors, setErrors] = useState({})
-    
-    const onSubmit = (fullName, email, phone, password, confirmPassword) =>{
-        const validationErrors = useValidateSignUp(fullName, email, phone, password, confirmPassword);
-        setErrors(validationErrors)
-        
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-        // if(Object.keys(validationErrors).length === 0){
-        //     console.log("HANDLE SIGN UP IS CALLED")
-        //     useHandleSignUp()
-        // }
-    }
+  const { handleSignUp, validationErrors, userCreationError, isLoading } = useHandleSignUp();
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen
-                options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
-                    headerShadowVisible: false,
-                    headerTitle: 'Sign Up'
-                }}
-            />
-            <View style={styles.margins}>
-                <View style={styles.header}>
-                    <Text style={styles.headerTitle}>Personal Information</Text>
-                </View>
-                <SignUpScreenInfo
-                    iconUrl={icons.user}
-                    fieldName="Full Name"
-                    isSecureText={false}
-                    onChangeText={setFullName}
-                    errorText={errors.fullName}
-                />
-                <SignUpScreenInfo
-                    iconUrl={icons.email}
-                    fieldName="Email"
-                    isSecureText={false}
-                    onChangeText={setEmail}
-                    errorText={errors.email}
-                />
-                <SignUpScreenInfo
-                    iconUrl={icons.phone}
-                    fieldName="Phone Number"
-                    isSecureText={false}
-                    onChangeText={setPhone}
-                    errorText={errors.phone}
-                />
-                <SignUpScreenInfo
-                    iconUrl={icons.padlock}
-                    fieldName="Password"
-                    isSecureText={true}
-                    onChangeText={setPassword}
-                    errorText={errors.password}
-                />
-                <SignUpScreenInfo
-                    iconUrl={icons.padlock}
-                    fieldName="Confirm Password"
-                    isSecureText={true}
-                    onChangeText={setConfirmPassword}
-                    errorText={errors.confirmPassword}
-                />
-                <LoginScreenBtn
-                    btnColour={COLORS.primary}
-                    handlePress={() =>  onSubmit(fullName, email, phone, password, confirmPassword)}
-                    text="Create Account"
-                />
+  const onSubmit = () => {
+    handleSignUp(fullName, email, phone, password, confirmPassword);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerTitle: 'Sign Up'
+        }}
+      />
+      {
+        isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : userCreationError ? (
+          <Text>Something Went Wrong: {userCreationError}</Text>
+        ) : (
+          <View style={styles.margins}>
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Personal Information</Text>
             </View>
-
-        </SafeAreaView>
-    );
+            <SignUpScreenInfo
+              iconUrl={icons.user}
+              fieldName="Full Name"
+              isSecureText={false}
+              onChangeText={setFullName}
+              errorText={validationErrors.fullName}
+            />
+            <SignUpScreenInfo
+              iconUrl={icons.email}
+              fieldName="Email"
+              isSecureText={false}
+              onChangeText={setEmail}
+              errorText={validationErrors.email}
+            />
+            <SignUpScreenInfo
+              iconUrl={icons.phone}
+              fieldName="Phone Number"
+              isSecureText={false}
+              onChangeText={setPhone}
+              errorText={validationErrors.phone}
+            />
+            <SignUpScreenInfo
+              iconUrl={icons.padlock}
+              fieldName="Password"
+              isSecureText={true}
+              onChangeText={setPassword}
+              errorText={validationErrors.password}
+            />
+            <SignUpScreenInfo
+              iconUrl={icons.padlock}
+              fieldName="Confirm Password"
+              isSecureText={true}
+              onChangeText={setConfirmPassword}
+              errorText={validationErrors.confirmPassword}
+            />
+            <LoginScreenBtn
+              btnColour={COLORS.primary}
+              handlePress={onSubmit}
+              text="Create Account"
+            />
+          </View>
+        )
+      }
+    </SafeAreaView>
+  );
 }
 
 export default SignUpScreen;
