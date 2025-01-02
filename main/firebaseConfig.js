14c57fe53faf18ce061221f09a68890770dde9e1
Carl Baseka <163749876+carlbaseka@users.noreply.github.com>
2025-01-01 20:33:44 -0500

    Removed useless code

--- main/firebaseConfig.js
+++ main/firebaseConfig.js
@@ -2,27 +2,25 @@ import { initializeApp } from "firebase/app";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { initializeFirestore} from "firebase/firestore";
+import { 
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, 
+  FIREBASE_MESSAGING_SENDER_ID, 
+  FIREBASE_APP_ID, 
+  FIREBASE_MEASUREMENT_ID 
+} from '@env';
 
 const firebaseConfig = {
-
-  ,
-
-  ,
-
-  ,
-
-  ,
-
-  ,
-
-  ,
-
-  
-
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
-
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
